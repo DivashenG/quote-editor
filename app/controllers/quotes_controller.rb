c63b4c3293721a60797f1957624707d8f86223e6
davidteren <drteren@gmail.com>
2022-03-20 21:09:50 +0200

    some fixes and add seeds

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,5 @@
 class QuotesController < ApplicationController
-  before_action :set_quote, only: %w[show edit update destroy]
+  before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
     @quotes = Quote.all
@@ -8,10 +8,14 @@ class QuotesController < ApplicationController
   def show
   end
 
+  def new
+    @quote = Quote.new
+  end
+
   def create
     @quote = Quote.new(quote_params)
 
-    if @quote.save!
+    if @quote.save
       redirect_to quotes_path, notice: "Quote was successfully created."
     else
       render :new, status: :unprocessable_entity
